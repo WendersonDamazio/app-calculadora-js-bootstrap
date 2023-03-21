68f0991c714803f9555f58e9d46ba84d089d7a69
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-03-21 09:25:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 
 function calcular(tipo, valor){
-    console.log(tipo, valor)
+    if(tipo === 'acao'){
+        if(valor === 'c'){
+            document.getElementById('resultado').value = ''    
+        }
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+        if(valor === '='){
+            var valor_campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valor_campo
+        }
+    }else if (tipo === 'valor'){
+        document.getElementById('resultado').value += valor
+    }
 }
